a4995dad69073b039358f2e5e0a64e27934963ae
Kate Windheuser <roboticbrain@gmail.com>
2014-10-14 09:11:51 -0600

    about halfway done, chunks with remainders don't work right yet.

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
 $(document).on('ready', function() {
   
 });
+
+function chunk (arr, numChunks) {
+	var myArray = arr;
+	var chunks = numChunks;
+	var totalNumValues = myArray.length;
+	var tempArray = [];
+	var finishedChunks = [];
+	if (totalNumValues % chunks === 0){
+		for (var i = 0; i < totalNumValues; i += chunks){
+			tempArray = myArray.slice(i, i + chunks);
+			finishedChunks.push(tempArray);
+		}
+	}
+	else {
+		var leftovers = totalNumValues % chunks;
+		
+	}
+	console.log(finishedChunks);
+	return finishedChunks;
+
+}
