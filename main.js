e6f2da96e7787760610204da7e9cd88bd689d778
Kate Windheuser <roboticbrain@gmail.com>
2014-10-14 12:14:47 -0600

    90 percent functional, just need to finish rendering chunks to the dom.

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 $(document).on('ready', function() {
-  
+	$(document).on('click', '.generategroups', function (){
+		var names = getNames();
+		console.log(names);
+		var numChunks = prompt('How many groups do you want?');
+		
+		console.log(doneChunks);
+		names = randomizer(names);
+		var doneChunks = chunk(names, numChunks);
+		renderGroups(names);
+	});
 });
 
 function chunk (arr, numChunks) {
@@ -8,17 +17,73 @@ function chunk (arr, numChunks) {
 	var totalNumValues = myArray.length;
 	var tempArray = [];
 	var finishedChunks = [];
-	if (totalNumValues % chunks === 0){
-		for (var i = 0; i < totalNumValues; i += chunks){
-			tempArray = myArray.slice(i, i + chunks);
+	var leftovers = totalNumValues % chunks;
+	var mainChunkNum = (totalNumValues - leftovers) / numChunks;
+	console.log(mainChunkNum);
+	for (var i = 0; i < totalNumValues; i+= newChunkNum){
+		if (leftovers > 0){
+			var newChunkNum = mainChunkNum + 1;
+			tempArray = myArray.slice(i, i+newChunkNum);
+			finishedChunks.push(tempArray);
+			leftovers--;
+		}
+		else {
+			var newChunkNum = mainChunkNum;
+			tempArray = myArray.slice(i, i+newChunkNum);
 			finishedChunks.push(tempArray);
 		}
 	}
-	else {
-		var leftovers = totalNumValues % chunks;
-		
-	}
 	console.log(finishedChunks);
 	return finishedChunks;
+}
+
+function getNames () {
+	var nameString = $('#nameinput').val();
+	console.log(nameString);
+	var nameArray = nameString.split(',');
+	$('.groupholder').empty();
+	return nameArray;
 
 }
+
+function randomizer (names) {
+	var namesTemp = names;
+	var randomize = prompt('Do you want them to be random groups?  Y/N');
+	if (randomize.toUpperCase() === 'Y'){
+		namesTemp = _.shuffle(namesTemp);
+		return namesTemp;
+	}
+	else {
+		return namesTemp;
+	}
+}
+
+function renderGroups (names) {
+	var namesTemp = names;
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
